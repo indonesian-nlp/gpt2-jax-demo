73744d7c6328d74039bcd54e89a9084423600c65
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2021-07-11 11:25:04 +0200

    add a prompt, fixed typos

--- streamlit/prompts.py
+++ streamlit/prompts.py
@@ -5,11 +5,12 @@ PROMPT_LIST = {
         "Berikut adalah bahan-bahan membuat nastar: "
     ],
     "Puisi (poetry)": [
-        "Aku ingin jadi merpati\nTerbat di langit yang damai\nBernyanyi-nyanyi tentang masa depan\n",
+        "Aku ingin jadi merpati\nTerbang di langit yang damai\nBernyanyi-nyanyi tentang masa depan\n",
         "Terdiam aku satu persatu dengan tatapan binar\nSenyawa merasuk dalam sukma membuat lara\nKefanaan membentuk kelemahan"
     ],
     "Cerpen (short story)": [
-        "Putri memakai sepatunya dengan malas. Kalau bisa, selama seminggu ini ia bolos sekolah saja. Namun, Bunda pasti akan marah. Ulangan tengah semester telah selesai. Minggu ini, di sekolah sedang beriangsung pekan olahraga.",
-        "\"Wah, hari ini cerah sekali ya,\" ucap Budi ketika ia keluar rumah."
+        "Putri memakai sepatunya dengan malas. Kalau bisa, selama seminggu ini ia bolos sekolah saja. Namun, Mama pasti akan marah. Ulangan tengah semester telah selesai. Minggu ini, di sekolah sedang berlangsung pekan olahraga.",
+        "\"Wah, hari ini cerah sekali ya,\" ucap Budi ketika ia keluar rumah.",
+        "Sewindu sudah kita tak berjumpa, rinduku padamu sudah tak terkira."
     ]
 }
